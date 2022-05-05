875feb371f25e996b6bcfc6521f99657097b69c0
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-05-05 12:21:11 -0400

    explicitly set the data type in image and header as float32; remove back_to_3d() which is duplicated

--- confixel/voxels.py
+++ confixel/voxels.py
@@ -22,26 +22,19 @@ def flattened_image(scalar_image, scalar_mask, group_mask_matrix):
     return scalar_matrix[group_mask_matrix].squeeze()     # .shape = (#voxels,)  # squeeze() is to remove the 2nd dimension which is not necessary
     
 
-def back_to_3d(group_mask_file, results_array, out_file):
-    """ Save a volume file for one statistical metric
-    """
-    group_mask_img = nb.load(group_mask_file)
-    group_mask_matrix = group_mask_img.get_fdata() > 0
-
-    output = np.zeros(group_mask_matrix.shape)
-    output[group_mask_matrix] = results_array
-    output_img = nb.Nifti1Image(output, affine=group_mask_img.affine,
-                                header=group_mask_img.header)
-    output_img.to_filename(out_file)
-
-
 def h5_to_volumes(h5_file, analysis_name, group_mask_file, output_extension, volume_output_dir):
     """ Convert stat results in .h5 file to a list of volume (.nii or .nii.gz) files
     """
 
+    data_type_tosave = np.float32
+
     # group-level mask:
     group_mask_img = nb.load(group_mask_file)
     group_mask_matrix = group_mask_img.get_fdata() > 0
+    
+    # modify the header:
+    header_tosave = group_mask_img.header
+    header_tosave.set_data_dtype(data_type_tosave)   # modify the data type (mask's data type could be uint8...)
 
     # results in .h5 file:
     h5_data = h5py.File(h5_file, "r")
@@ -66,9 +59,11 @@ def h5_to_volumes(h5_file, analysis_name, group_mask_file, output_extension, vol
 
         out_file = op.join(volume_output_dir, analysis_name + "_" + valid_result_name + output_extension)
         output = np.zeros(group_mask_matrix.shape)
-        output[group_mask_matrix] = results_matrix[result_col, :]
+        data_tosave = results_matrix[result_col, :]
+        data_tosave = data_tosave.astype(data_type_tosave)  # make sure each result image's data type is the correct one
+        output[group_mask_matrix] = data_tosave
         output_img = nb.Nifti1Image(output, affine=group_mask_img.affine,
-                                    header=group_mask_img.header)
+                                    header=header_tosave)
         output_img.to_filename(out_file)
 
         # if this result is p.value, also write out 1-p.value (1m.p.value)
@@ -76,9 +71,11 @@ def h5_to_volumes(h5_file, analysis_name, group_mask_file, output_extension, vol
             valid_result_name_1mpvalue = valid_result_name.replace("p.value", "1m.p.value")
             out_file_1mpvalue = op.join(volume_output_dir, analysis_name + "_" + valid_result_name_1mpvalue + output_extension)
             output_1mpvalue = np.zeros(group_mask_matrix.shape)
-            output_1mpvalue[group_mask_matrix] = 1 - results_matrix[result_col, :]  # 1 minus
+            data_tosave = 1 - results_matrix[result_col, :]    # 1 minus
+            data_tosave = data_tosave.astype(data_type_tosave)  # make sure each result image's data type is the correct one
+            output_1mpvalue[group_mask_matrix] = data_tosave 
             output_img_1mpvalue = nb.Nifti1Image(output_1mpvalue, affine=group_mask_img.affine,
-                                                header=group_mask_img.header)
+                                                header=header_tosave)
             output_img_1mpvalue.to_filename(out_file_1mpvalue)
 
 
